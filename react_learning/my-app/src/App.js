0cdbd93c07296911a3ff5caebff6ddcc81236e42
Ruchi Patel <UP0062@uplers.local>
2022-04-20 13:43:36 +0530

    first commit

---
+++ react_learning/my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <>
+    {/*<Navbar title="TextUtils" homemenu="Home" aboutmenu="About"></Navbar>*/}
+    <Navbar/>
+    </>
+  )
+}
+
+export default App;
